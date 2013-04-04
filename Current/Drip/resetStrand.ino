cfe39238c4146bc1164bd6542db76f7dc7f5845e
Jesse Scott <jesse@memelab.ca>
2013-04-04 00:27:51 -0700

    resetStrand pt 2

--- Current/Drip/resetStrand.ino
+++ Current/Drip/resetStrand.ino
@@ -4,26 +4,34 @@
 
 void resetStrand(int index) {
   
+  // Is It At the End ?? Then Reset
   if (k >= endLED[index]){
     t[index] = s[index];
   }
   
-  if(currentMillis[index] - stepTime[index] >= t[index] ){
+  // Is Our Timer Ready To Switch ?? 
+  if(currentMillis[index] - stepTime[index] >= t[index]) {
+    
+    // Turn The LEDs Black
     leds[LEDsections[index]].r = 0;
     leds[LEDsections[index]].g = 0;
     leds[LEDsections[index]].b = 0;
 
+    // Increment
     LEDsections[index]++;
 
+    // Go Back To The Beginning
     if(LEDsections[index] == endLED[index]){
       LEDsections[index] = startLED[index];
       t[index] = s[index];
     }
 
+    // Turn The LEDs White
     leds[LEDsections[index]].r = 255;
     leds[LEDsections[index]].g = 255;
     leds[LEDsections[index]].b = 255;
 
+    // Reset Our Timer
     stepTime[index] = currentMillis[index];
   }
   
