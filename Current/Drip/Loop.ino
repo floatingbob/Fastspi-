d06d83b447e197757a3a0b6df02c715aaadd02b3
Jesse Scott <jesse@memelab.ca>
2013-04-04 01:31:33 -0700

    Moved Speed to for loop

--- Current/Drip/Loop.ino
+++ Current/Drip/Loop.ino
@@ -8,20 +8,23 @@ void loop() {
   int maX = 35;          
   int miN = 1;
   
-  // Blink Speed
-  int speed[9] = {
-    random(blinkSpeed[miN],blinkSpeed[maX]), //0
-    random(blinkSpeed[miN],blinkSpeed[maX]), //1
-    random(blinkSpeed[miN],blinkSpeed[maX]), //2
-    random(blinkSpeed[miN],blinkSpeed[maX]), //3
-    random(blinkSpeed[miN],blinkSpeed[maX]), //4
-    random(blinkSpeed[miN],blinkSpeed[maX]), //5
-    random(blinkSpeed[miN],blinkSpeed[maX]), //6
-    random(blinkSpeed[miN],blinkSpeed[maX]), //7 
-    random(blinkSpeed[miN],blinkSpeed[maX])  //8
-  };
+//  // Blink Speed
+//  speed[9] = {
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //0
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //1
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //2
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //3
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //4
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //5
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //6
+//    random(blinkSpeed[miN],blinkSpeed[maX]), //7 
+//    random(blinkSpeed[miN],blinkSpeed[maX])  //8
+//  };
 
   for(int i = 0; i <= numStrands; i++) {
+    
+    // Speed
+    speed[i] = random(blinkSpeed[miN],blinkSpeed[maX]);
     
     // Reset Current Millisecond
     currentMillis[i] = millis();
