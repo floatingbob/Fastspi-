21f96ddf871cc0e55f44f405fc7c1e69219f4e9a
Jesse Scott <jesse@memelab.ca>
2013-04-13 13:44:42 -0700

    Ignore First Strand

--- Current/Drip/resetStrand.ino
+++ Current/Drip/resetStrand.ino
@@ -3,38 +3,39 @@
 // RESET
 
 void resetStrand(int k, int i) {
-  
+  // Ignore First Stand
   if(i != 1) {
-    if (k >= endLED[i]){       
-        timer[i] = speed[i];
-     } 
-  }
-  
-  // Is Our Timer Ready To Switch ?? 
-  if(currentMillis[i] - stepTime[i] >= timer[i]) {
     
-    // Turn The LEDs Black
-    leds[LEDsections[i]].r = 0;
-    leds[LEDsections[i]].g = 0;
-    leds[LEDsections[i]].b = 0;
-
-    // Increment
-    LEDsections[i]++;
-
-    // Go Back To The Beginning
-    if(LEDsections[i] == endLED[i]){
-      LEDsections[i] = startLED[i];
+    if (k >= endLED[i]){       
       timer[i] = speed[i];
-    }
-
-    // Turn The LEDs White
-    leds[LEDsections[i]].r = 255;
-    leds[LEDsections[i]].g = 255;
-    leds[LEDsections[i]].b = 255;
-
-    // Reset Our Timer
-    stepTime[i] = currentMillis[i];
-  }
+    } 
+    
+    // Is Our Timer Ready To Switch ?? 
+    if(currentMillis[i] - stepTime[i] >= timer[i]) {
+      
+      // Turn The LEDs Black
+      leds[LEDsections[i]].r = 0;
+      leds[LEDsections[i]].g = 0;
+      leds[LEDsections[i]].b = 0;
   
+      // Increment
+      LEDsections[i]++;
+  
+      // Go Back To The Beginning
+      if(LEDsections[i] == endLED[i]){
+        LEDsections[i] = startLED[i];
+        timer[i] = speed[i];
+      }
+  
+      // Turn The LEDs White
+      leds[LEDsections[i]].r = 255;
+      leds[LEDsections[i]].g = 255;
+      leds[LEDsections[i]].b = 255;
+  
+      // Reset Our Timer
+      stepTime[i] = currentMillis[i];
+    }
+  
+  }
   
 }
